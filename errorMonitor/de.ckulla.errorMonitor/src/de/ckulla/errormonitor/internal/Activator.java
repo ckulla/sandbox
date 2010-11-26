afac687244c848f24399acb2a203241a1f130331
Christoph Kulla <christophkulla@googlemail.com>
2010-11-26 21:46:58 +0100

    Initial import of the errMonitor project. Displays number of errors in workspace as overlay in the dock.

---
+++ errorMonitor/de.ckulla.errorMonitor/src/de/ckulla/errormonitor/internal/Activator.java
@@ -0,0 +1,47 @@
+package de.ckulla.errormonitor.internal;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+	
+	private static ErrorMonitor errorMonitor;
+	
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		if (errorMonitor!=null) {
+			errorMonitor.dispose();
+		}
+		Activator.context = null;
+	}
+
+	public static synchronized ErrorMonitor getErrorMonitor () {
+		if (errorMonitor==null) {
+			errorMonitor = new ErrorMonitor();
+		}
+		return errorMonitor;
+	}
+
+}
---
+++ errorMonitor/de.ckulla.errorMonitor/src/de/ckulla/errormonitor/internal/ErrorMonitor.java
@@ -0,0 +1,96 @@
+package de.ckulla.errormonitor.internal;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IMarkerDelta;
+import org.eclipse.core.resources.IResourceChangeEvent;
+import org.eclipse.core.resources.IResourceChangeListener;
+import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.swt.widgets.TaskBar;
+import org.eclipse.swt.widgets.TaskItem;
+import org.eclipse.ui.IStartup;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.views.markers.internal.ProblemMarker;
+
+@SuppressWarnings("restriction")
+public class ErrorMonitor implements IResourceChangeListener, IStartup {
+
+	Set<Long> markerIds = new HashSet<Long>();
+	
+	static TaskItem getTaskBarItem() {
+		TaskBar bar = PlatformUI.getWorkbench().getDisplay().getSystemTaskBar();
+		if (bar == null)
+			return null;
+		TaskItem item = bar.getItem(PlatformUI.getWorkbench()
+				.getActiveWorkbenchWindow().getShell());
+		if (item == null)
+			item = bar.getItem(null);
+		return item;
+	}
+
+	public void earlyStartup() {
+		MarkerFilter filter = new ProblemFilter("all problems");
+		try {
+			@SuppressWarnings("unchecked")
+			final Collection<ProblemMarker> markers = filter.findMarkers(new NullProgressMonitor(), true);
+			if (markers != null) {
+				for (ProblemMarker marker: markers) {
+					if (marker.getSeverity()==IMarker.SEVERITY_ERROR)
+						markerIds.add (marker.getMarker().getId());
+				}
+			}
+			update();
+		} catch (CoreException e) {
+			e.printStackTrace();
+		}
+		ResourcesPlugin.getWorkspace().addResourceChangeListener(
+				this,
+				IResourceChangeEvent.POST_CHANGE);
+//						| IResourceChangeEvent.PRE_BUILD
+//						| IResourceChangeEvent.POST_BUILD);
+	}
+
+	public void dispose() {
+		ResourcesPlugin.getWorkspace().removeResourceChangeListener(this);
+	}
+
+	@Override
+	public void resourceChanged(IResourceChangeEvent event) {
+		IMarkerDelta[] deltas = event.findMarkerDeltas(IMarker.PROBLEM, true);
+		if (deltas != null) {
+			for (IMarkerDelta delta : deltas) {
+				switch (delta.getKind()) {
+				case IResourceDelta.ADDED:
+					if (delta.getAttribute(IMarker.SEVERITY, -1)==IMarker.SEVERITY_ERROR)
+						markerIds.add (delta.getId());
+					break;
+				case IResourceDelta.REMOVED:
+					if (delta.getAttribute(IMarker.SEVERITY, -1)==IMarker.SEVERITY_ERROR)
+						markerIds.remove (delta.getId());
+					break;
+				}
+			}
+			update();
+		}
+	}
+
+	private void update() {
+		final IWorkbench workbench = PlatformUI.getWorkbench();
+		workbench.getDisplay().asyncExec(new Runnable() {
+			public void run() {
+				if (markerIds!=null && markerIds.size() > 0) {
+					getTaskBarItem().setOverlayText("" + markerIds.size());
+				} else {
+					getTaskBarItem().setOverlayText("");
+				}
+			}
+		});
+	}
+}
